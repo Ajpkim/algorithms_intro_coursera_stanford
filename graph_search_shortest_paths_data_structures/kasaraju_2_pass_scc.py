bb3d4e60a5318f693b08f8d3cbe084c28cb2a939
Alexander <alexkim345@gmail.com>
2020-04-10 19:41:51 -0400

    passing small test cases, recursion depth issues

--- graph_search_shortest_paths_data_structures/kasaraju_2_pass_scc.py
+++ graph_search_shortest_paths_data_structures/kasaraju_2_pass_scc.py
@@ -1,44 +1,83 @@
-"""
-Output Format: You should output the sizes of the 5 largest SCCs in the given graph, in decreasing order of sizes, separated by commas (avoid any spaces). So if your algorithm computes the sizes of the five largest SCCs to be 500, 400, 300, 200 and 100, then your answer should be "500,400,300,200,100" (without the quotes). If your algorithm finds less than 5 SCCs, then write 0 for the remaining terms. Thus, if your algorithm computes only 3 SCCs whose sizes are 400, 300, and 100, then your answer should be "400,300,100,0,0" (without the quotes). (Note also that your answer should not have any spaces in it.)
-"""
+import sys
+import time
 
-filename = 'SCC_assignment_graph.txt'
+# USE THREADS
+# USE STACK FOR DFS?
+
+
+def main(filename):
+    SCCs = kasaraju_2_pass_SCC(filename)
+    print("Number of nodes in each SCC:", SCCs)
 
 
 def kasaraju_2_pass_SCC(filename):
+    print('endering algo', flush=True)
     g = load_graph_dict(filename)
-    g_rev = reverse_graph_dict(graph)
+    g_rev = reverse_graph_dict(g)
+    print('got the graphs', flush=True)
 
+    # print('g:', g)
+    # print('g_rev:', g_rev)
+    # print("")
+    # global explored_nodes
+    global leaders
+    global finishing_times
+    # explored_nodes = []
     finishing_times = []  # nodes ordered by ascending finishing time i.e. 0th node is lowest finishing time
     leaders = {}
+
+    print('about to enter 1st loop', flush=True)
     DFS_loop(g_rev)
 
-    finishing_times = finishing_times.reverse()  # reverse list to put largest finishing times in front
+    finishing_times.reverse()  # reverse list in-place to put largest finishing times in front
+    # print('nodes by finishing times', finishing_times)
 
     g_ordered = {}  # dicts are ordered in python 3.6+
+    # logic for ensuring that nodes without any outgoing edges are still correctly DFS'ed on
     for node in finishing_times:
-        g_ordered[node] = g[node]
+        try:
+            g_ordered[node] = g[node]
+        except:
+            g_ordered[node] = []
+
+    # print("----------------------------------------------")
+
+    # print("g_ordered:", g_ordered)
+    # print('')
 
     leaders = {}  # reset var
+
+    print('about to enter 2nd loop', flush=True)
+
     DFS_loop(g_ordered)
 
-    sccs = [leaders[scc] for scc in leaders]
+    sccs = [len(leaders[scc]) for scc in leaders]
+    # print('leaders dict:', leaders)
 
     return sccs
 
 
 def DFS_loop(graph):
-    global t
+    print('inside DFS LOOP', flush=True)
+    global leaders
     global leader
     global explored_nodes
-
+    global t
     t = 0
     leader = None
     explored_nodes = []
 
+    # debugging
+    global recursion_counter
+    recursion_counter = 0
+
     for node in graph:
+        # print('\nDFS LOOPING ON NODE:', node)
         if node not in explored_nodes:
+
             leader = node
+            leaders[leader] = []
+
             DFS(graph, node)
 
 
@@ -49,15 +88,37 @@ def DFS(graph, node):
     global leaders
     global explored_nodes
 
+    # debugging
+    global recursion_counter
+    recursion_counter += 1
+    print(f'DFS: {recursion_counter}', flush=True)
+
+    # print('')
+    # print('dfsing on node:', node)
+    # print('current explored_nodes:', explored_nodes)
+
+    # print('adding node to explored_nodes')
     explored_nodes.append(node)
+    # print('explored_nodes:', explored_nodes)
     leaders[leader].append(node)
 
-    for head in g_rev[node]:
-        if head not in explored_nodes:
-            DFS(graph, head)
-
+    # handle cases when node has no outgoing edges (g_rev cases)
+    if node not in graph:
+        # print('adding node with no outgoing edges to ft')
         finishing_times.append(node)
-        t += 1
+        return
+
+    for head_node in graph[node]:
+        if head_node not in explored_nodes:
+            DFS(graph, head_node)
+    # print('')
+    # print('...')
+    # print('end of DFS for node:', node)
+    # print('current finishing_times:', finishing_times)
+    # print('adding to finishing_times')
+    finishing_times.append(node)
+    # print('finishing_times:', finishing_times)
+    t += 1
 
 
 def load_graph_dict(filename):
@@ -67,7 +128,7 @@ def load_graph_dict(filename):
     with open(filename, 'r') as f:
         for line in f:
             edge = line.split(' ')
-            tail, head = edge[0], edge[1]
+            tail, head = int(edge[0]), int(edge[1])  # convert to int for test cases
             if tail not in graph:
                 graph[tail] = [head]
             else:
@@ -85,3 +146,21 @@ def reverse_graph_dict(graph):
             else:
                 g_rev[v].append(k)
     return g_rev
+
+
+# test cases:
+# filename = "kasaraju_test_cases/33300.txt"  # success
+# filename = "kasaraju_test_cases/33200.txt"  # success
+# filename = "kasaraju_test_cases/63210.txt"  # success
+# filename = "kasaraju_test_cases/31100.txt"  # success
+
+# When I set the recursionlimit to a high number the call just passes out on the command line...
+
+sys.setrecursionlimit(10**7)
+filename = 'SCC_assignment_graph.txt'
+# print(sys.getrecursionlimit())
+if __name__ == '__main__':
+    start = time.perf_counter()
+    main(filename)
+    end = time.perf_counter()
+    print(end-start, 'seconds')
--- graph_search_shortest_paths_data_structures/worksheet.py
+++ graph_search_shortest_paths_data_structures/worksheet.py
@@ -1,23 +1,57 @@
+import sys
+from kasaraju_2_pass_scc import *
 
-leaders = {}
-leader = None
+sys.setrecursionlimit(10**6)
+# print(sys.getrecursionlimit())
+# filename = "kasaraju_test_cases/33300.txt"  # success
+# filename = "kasaraju_test_cases/33200.txt"  # success
+# filename = "kasaraju_test_cases/63210.txt"  # success
+filename = "kasaraju_test_cases/31100.txt"  # success
+# filename = "kasaraju_test_cases/32_10.txt"
+
+# filename = "SCC_assignment_graph.txt"
+
+# g = load_graph_dict(filename)
+# print(g)
+#
+# g_rev = reverse_graph_dict(g)
+# print(g_rev)
+
+res = kasaraju_2_pass_SCC(filename)
+print(res)
 
 
-def wrapper():
-    global leader
-    leader = 3
+# a = [i for i in range(10)]
+# print(a)
+#
+# a.reverse()
+# print(a)
 
-    add_to_leaders()
-
-
-def add_to_leaders():
-    global leader
-    global leaders
-    leaders[leader] = 333
-
-
-wrapper()
-print(leaders)
+# leaders = {}
+# leader = None
+#
+#
+# def wrapper():
+#     global leader
+#     leader = 3
+#     # global igloo
+#     igloo = 10
+#
+#     add_to_leaders()
+#
+#
+# def add_to_leaders():
+#     global igloo
+#     igloo = 44
+#
+#     global leader
+#     global leaders
+#     leaders[leader] = 339
+#
+#
+# wrapper()
+# print(leaders)
+# print(igloo)
 
 
 # a = [[111, 11, 122], [11, 111], [12, 122], [13, 133], [14, 444], [15, 555]]
