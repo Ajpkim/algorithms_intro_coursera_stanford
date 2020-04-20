f4b6b45b1641ba0ec4e695a426258642496977aa
ajpkim <alexkim345@gmail.com>
2020-04-19 22:24:36 -0400

    begain heap dijkstra version

--- graph_search_shortest_paths_data_structures/kasaraju/kasaraju_2_pass_scc.py
+++ graph_search_shortest_paths_data_structures/kasaraju/kasaraju_2_pass_scc.py
@@ -16,21 +16,18 @@ def kasaraju_2_pass_SCC(filename):
     g_rev = reverse_graph_dict(g)
     print('got the graphs', flush=True)
 
-    # print('g:', g)
-    # print('g_rev:', g_rev)
-    # print("")
-    # global explored_nodes
     global leaders
     global finishing_times
     # explored_nodes = []
-    finishing_times = []  # nodes ordered by ascending finishing time i.e. 0th node is lowest finishing time
+    # nodes ordered by ascending finishing time i.e. 0th node is lowest finishing time
+    finishing_times = []
     leaders = {}
 
     print('about to enter 1st loop', flush=True)
     DFS_loop(g_rev)
 
-    finishing_times.reverse()  # reverse list in-place to put largest finishing times in front
-    # print('nodes by finishing times', finishing_times)
+    # reverse list in-place to put largest finishing times in front
+    finishing_times.reverse()
 
     g_ordered = {}  # dicts are ordered in python 3.6+
     # logic for ensuring that nodes without any outgoing edges are still correctly DFS'ed on
@@ -40,10 +37,6 @@ def kasaraju_2_pass_SCC(filename):
         except:
             g_ordered[node] = []
 
-    # print("----------------------------------------------")
-
-    # print("g_ordered:", g_ordered)
-    # print('')
 
     leaders = {}  # reset var
 
@@ -52,7 +45,6 @@ def kasaraju_2_pass_SCC(filename):
     DFS_loop(g_ordered)
 
     sccs = [len(leaders[scc]) for scc in leaders]
-    # print('leaders dict:', leaders)
 
     return sccs
 
@@ -67,12 +59,10 @@ def DFS_loop(graph):
     leader = None
     explored_nodes = []
 
-    # debugging
     global recursion_counter
     recursion_counter = 0
 
     for node in graph:
-        # print('\nDFS LOOPING ON NODE:', node)
         if node not in explored_nodes:
 
             leader = node
@@ -88,36 +78,22 @@ def DFS(graph, node):
     global leaders
     global explored_nodes
 
-    # debugging
     global recursion_counter
     recursion_counter += 1
     print(f'DFS: {recursion_counter}', flush=True)
 
-    # print('')
-    # print('dfsing on node:', node)
-    # print('current explored_nodes:', explored_nodes)
-
-    # print('adding node to explored_nodes')
     explored_nodes.append(node)
-    # print('explored_nodes:', explored_nodes)
     leaders[leader].append(node)
 
     # handle cases when node has no outgoing edges (g_rev cases)
     if node not in graph:
-        # print('adding node with no outgoing edges to ft')
         finishing_times.append(node)
         return
 
     for head_node in graph[node]:
         if head_node not in explored_nodes:
             DFS(graph, head_node)
-    # print('')
-    # print('...')
-    # print('end of DFS for node:', node)
-    # print('current finishing_times:', finishing_times)
-    # print('adding to finishing_times')
     finishing_times.append(node)
-    # print('finishing_times:', finishing_times)
     t += 1
 
 
@@ -128,7 +104,8 @@ def load_graph_dict(filename):
     with open(filename, 'r') as f:
         for line in f:
             edge = line.split(' ')
-            tail, head = int(edge[0]), int(edge[1])  # convert to int for test cases
+            # convert to int for test cases
+            tail, head = int(edge[0]), int(edge[1])
             if tail not in graph:
                 graph[tail] = [head]
             else:
